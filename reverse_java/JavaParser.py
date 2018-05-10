355b3f404ccd384cca9f5d5fda8d203d69d9b811
alexad19 <drumea.alexandru.daniel@gmail.com>
2018-05-11 02:14:33 +0300

    added reverse_java complete with the UML generation

--- reverse_java/JavaParser.py
+++ reverse_java/JavaParser.py
@@ -1,12 +1,20 @@
 import os
-root_dir = r'C:\dir' 
+import sys
+import dict2uml
+import json
+import plantuml
+
+
+root_dir = sys.argv[1]
 
 
 def get_info():
     classes = []
     interfaces = []
     implementations = []
-    extensions = []
+    
+    relations = []
+    found_list = []
 
     for folder, dirs, files in os.walk(root_dir):
         for file in files:
@@ -15,20 +23,35 @@ def get_info():
                 with open(full_path, 'r') as f:
                     for line in f:
                         if "class " in line:
-                            if " extends " not in line:
-                                line = line.split(' ', 1)[1]
-                                classes += [line.rstrip()]
+                            class_line = line.split()[2]
+#                             class_line = line.strip('\n')
+                            classes += [class_line]
+
+                            temp_dict = {class_line : {}}
+
+                            if "extends" in line:
+                                implementations += [name.replace(',', '') for name in line.rsplit()[3:]]
+                                for name in implementations:
+                                    if name in found_list:
+                                        pass
+                                    else:
+                                        impl_dict = {name:temp_dict}
+                                        found_list.append(name)
+                                        relations.append(impl_dict)
                             else:
-                                implementations += [line.rstrip()]
-                        elif "interface " in line:
-                            line = line.split(' ', 1)[1]
-                            interfaces += [line.rstrip()]
+                                relations.append(temp_dict)
+                                found_list.append(class_line)
 
-    return classes, \
-        interfaces,\
-        implementations,\
-        extensions
-
+                        
+    return relations
 
 if __name__ == "__main__":
-    print(get_info())
+    info = get_info()
+    print(info)
+     
+    uml = plantuml.PlantUML()
+    uml_no = 1
+    for d in info:
+        with open(str(uml_no) + '.png', 'wb') as out:
+            out.write(uml.processes(dict2uml.dict2plantuml(d)))
+        uml_no += 1
