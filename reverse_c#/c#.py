4fa9f2b986e1119b9b38ae4cef0d22835e22041b
Cosmin Poenaru <cpoenaru@bitdefender.com>
2018-05-15 10:02:27 +0300

    ++ concat images

--- reverse_c#/c#.py
+++ reverse_c#/c#.py
@@ -4,7 +4,6 @@ import dict2uml
 import json
 import plantuml
 
-
 root_dir = sys.argv[1]  # r'C:\Users\cpoenaru\Desktop\f\IP'
 
 
@@ -12,7 +11,7 @@ def get_info():
     classes = []
     interfaces = []
     implementations = []
-    
+
     relations = []
     found_list = []
 
@@ -22,11 +21,14 @@ def get_info():
                 full_path = os.path.join(folder, file)
                 with open(full_path, 'r') as f:
                     for line in f:
+                        if "// TODO" in line:
+                            continue
+
                         if "class " in line:
-                            class_line = line.rsplit()[1]
+                            class_line = line.rsplit()[2]
                             classes += [class_line]
 
-                            temp_dict = {class_line : {}}
+                            temp_dict = {class_line: {}}
 
                             if ":" in line:
                                 implementations += [name.replace(',', '') for name in line.rsplit()[3:]]
@@ -34,30 +36,49 @@ def get_info():
                                     if name in found_list:
                                         pass
                                     else:
-                                        impl_dict = {name:temp_dict}
+                                        impl_dict = {name: temp_dict}
                                         found_list.append(name)
                                         relations.append(impl_dict)
                             else:
                                 relations.append(temp_dict)
                                 found_list.append(class_line)
 
-                        #elif "interface " in line:
+                        # elif "interface " in line:
                         #    line = line.rsplit()[1]
                         #    interfaces += [line]
 
-    #classes, \
-        #interfaces,\
-        #implementations
+    # classes, \
+    # interfaces,\
+    # implementations
     return relations
+
+
+def concat_images(images):
+    from PIL import Image
+    images = map(Image.open, images)
+    widths, heights = zip(*(i.size for i in images))
+    total_width = sum(widths)
+    max_height = max(heights)
+    new_im = Image.new('RGB', (total_width, max_height), "white")
+    x_offset = 0
+    for im in images:
+        new_im.paste(im, (x_offset, 0))
+        x_offset += im.size[0]
+    new_im.save('final_diagram.png')
 
 
 if __name__ == "__main__":
     info = get_info()
     print(info)
-
+    to_concat = []
     uml = plantuml.PlantUML()
-    uml_no = 1
+    uml_no = 0
     for d in info:
-        with open(str(uml_no) + '.png', 'wb') as out:
-            out.write(uml.processes(dict2uml.dict2plantuml(d)))
         uml_no += 1
+        file_name = (str(uml_no) + '.png')
+        with open(file_name, 'wb') as out:
+            out.write(uml.processes(dict2uml.dict2plantuml(d)))
+        if os.path.isfile(file_name):
+            to_concat += [file_name]
+
+    concat_images(to_concat)
