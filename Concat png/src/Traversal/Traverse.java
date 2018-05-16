96da9609e27be045db68f410adbc5d31ad574155
alexad19 <drumea.alexandru.daniel@gmail.com>
2018-05-16 08:57:36 +0300

    added all folder traversal method

---
+++ Concat png/src/Traversal/Traverse.java
@@ -0,0 +1,31 @@
+package Traversal;
+
+import java.io.File;
+import java.io.IOException;
+
+import Concatenation.*;
+
+public class Traverse {
+
+        static Concat c = new Concat();
+        public static void main (String args[]) throws IOException {
+
+            displayIt(new File("D:\\Anul 2\\ProiectIP\\ProiectIPM3"));
+        }
+
+        public static void displayIt(File node) throws IOException {
+
+
+
+            if(node.isDirectory() && !node.getName().startsWith(".")){
+//                System.out.println(node.getAbsoluteFile());
+                String[] subNote = node.list();
+                for(String filename : subNote){
+//                    System.out.println(filename);
+                    displayIt(new File(node, filename));
+                    c.run(node.getAbsoluteFile());
+                }
+            }
+            else return;
+        }
+}
