277add6af1402f2753f99d18c558d4ccba7a5e50
andrachiorcea <achiorcea27@gmail.com>
2018-05-16 08:25:44 +0300

    Added testing methods for sorting plus did refactoring on the sorting classes

--- sorting/SortingPartIPM3/src/main/SortByNumberOfStars.java
+++ sorting/SortingPartIPM3/src/main/SortByNumberOfStars.java
@@ -1,14 +1,18 @@
+
 import com.jcabi.github.*;
 import com.jcabi.http.response.JsonResponse;
 
 import javax.json.JsonArray;
 import javax.json.JsonObject;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SortByNumberOfStars {
-    public static void sortDescNumberOfStars(){
+    public static Map<JsonObject, Integer> sortDescNumberOfStars(){
         final Github github = new RtGithub();
+        Map<JsonObject, Integer> list = new HashMap<JsonObject, Integer>();
         try {
             final JsonResponse resp = github.entry()
                     .uri().path("/search/repositories")
@@ -22,12 +26,13 @@ public class SortByNumberOfStars {
                     .getValuesAs(JsonObject.class);
             for (final JsonObject item : items) {
                 String name="/repos/"+item.get("full_name").toString()+"/stargazers";
-
                 JsonResponse response = github.entry()
                         .uri().path(name.replace("\"","")).back()
                         .fetch()
                         .as(JsonResponse.class);
                 JsonArray stargazers = response.json().readArray();
+                int stargazers1= response.json().readArray().size();
+                list.put(item,stargazers1);
                 System.out.println(
                         String.format(
                                 "repository found: %s",
@@ -40,6 +45,7 @@ public class SortByNumberOfStars {
         catch (IOException e) {
             System.out.println("Problem encountered");
         }
+        return list;
     }
 }
 
