691c5e79c7610d2d68b4ad648f01520f0171b81e
Gospodaru Bogdan <bgospodaru@bitdefender.com>
2018-05-23 08:45:23 +0300

    documentation

---
+++ reverse_c#/Tests/no_class/FileData.cs
@@ -0,0 +1,17 @@
+﻿using EasyHttp.Http;
+
+namespace EasyHttp.Infrastructure
+{
+    public test FileData
+    {
+        public string FieldName { get; set; }
+        public string Filename { get; set; }
+        public string ContentType { get; set; }
+        public string ContentTransferEncoding { get; set; }
+
+        public FileData()
+        {
+            ContentTransferEncoding = HttpContentTransferEncoding.Binary;
+        }
+    }
+}
